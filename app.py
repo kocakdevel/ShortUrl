8d4b9fb2cf23aabaacdcc65db6680fb4d357b9ec
Immanuel Mruk <immanuelmruk@gmail.com>
2024-09-25 15:53:01 +0200

    Add database models

--- app.py
+++ app.py
@@ -1,22 +1,29 @@
 from flask import Flask, render_template
 from dotenv import load_dotenv
-from flask_sqlalchemy import SQLAlchemy
+from models import db, User, ShortUrl
 import os
-
 
 load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv('FLASK_SECRET_KEY')
-app.config['SQLALCHMEY_DATABASE_URI'] = 'sqlite:///app.db' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+db.init_app(app)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+def init_db():
+    with app.app_context():
+        db.create_all()
+        app.logger.info("Database initialized")
+
 try:
+    init_db()
     app.run(debug=True, host='0.0.0.0', port=8080)
 except Exception as e:
     app.logger.error(f"App startup error: {str(e)}")
-
---
+++ models.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from werkzeug.security import generate_password_hash, check_password_hash
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    files = relationship('File', backref='uploader', lazy=True)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+
+class ShortUrl(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    original_url = db.Column(db.String(2083), nullable=False)
+    custom_url = db.Column(db.String(120), unique=True, nullable=False)
+    views = db.Column(db.Integer, default=0)
+    created_by = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
+if __name__ == '__main__':
+    db.create_all()
